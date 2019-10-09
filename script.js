60fa2740c42221f61af607752669deb11d5c1b4d
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2019-10-09 18:46:27 +0300

    Заливка в репозиторий

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    margin: 0,
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    autoplayTimeout: 5000,
+
+  });
+});
